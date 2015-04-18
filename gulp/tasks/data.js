1a004cef4ba3a73b7484c1a7678cf4cc009ca631
Jared Sartin <jaredsartin@gmail.com>
2015-04-17 22:22:30 -0700

    Update 2015-04-18T05:22:28.229Z

--- gulp/tasks/data.js
+++
@@ -1,9 +0,0 @@
-var changed    = require('gulp-changed');
-var gulp       = require('gulp');
-var config     = require('../config').data;
-
-gulp.task('data', function() {
-  return gulp.src(config.src)
-    .pipe(changed(config.dest)) // Ignore unchanged files
-    .pipe(gulp.dest(config.dest));
-});
